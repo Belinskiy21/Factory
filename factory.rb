62ec09d17ded0a1561ec837788b0dee9d6fdd404
Oleg <belinskiy21@gmail.com>
2017-05-11 13:04:44 +0300

    check for capitalize

--- factory.rb
+++ factory.rb
@@ -1,11 +1,17 @@
 class Factory
   def self.new(*args, &block)
-    Class.new do
+
+     args.first.class == String and args.first[0] == args.first[0].capitalize ? name = args.shift : args.shift
+
+    new_class = Class.new do
+      
       attr_accessor  *args
 
 
-      define_method(:initialize) do |*vars|
-        vars.each_with_index { |v, i| send("#{args[i]}=", v) }
+      define_method :initialize do |*attr|
+        args.each_with_index do |value, index|
+          self.instance_variable_set("@#{value}", attr[index])
+        end
       end
 
       def [](value)
@@ -24,5 +30,6 @@ class Factory
       
 
     end
+     name ? const_set(name, new_class) : new_class
   end
 end
